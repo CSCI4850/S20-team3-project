ae99bf4d0fc15492d5530724ab1c1c9c8874458d
vat880 <jbg4a@mtmail.mtsu.edu>
2020-03-25 16:39:26 -0500

    Initial convonet and pulled ReplayMemory from UniformReplayMemory for testing if necessary

--- SingleQ/Uniform/GalagaAgent.py
+++ SingleQ/Uniform/GalagaAgent.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+
+import keras
+
+# ten enemy types and one projectile type so 11 feature detectors
+
+class GalagaAgent:
+        def __init__(self, state_size, action_size):
+            self.state_size = state_size
+            self.action_size = action_size
+
+        def make_model(state_size, action_size):
+            model = keras.Sequential()
+            model.add(keras.layers.Conv2D(11, kernel_size=(15, 15), activation='relu', input_shape=state_size))
+            model.add(keras.layers.Conv2D(22, (15, 15), activation='relu'))
+            model.add(keras.layers.MaxPooling2D(pool_size=(3, 3)))
+            model.add(keras.layers.Dropout(.20))
+            model.add(keras.layers.Flatten())
+            model.add(keras.layers.Dense(22, activation='relu'))
+            model.add(keras.layers.Dense(action_size, activation='softmax'))
+            model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Nadam(learning_rate = 0.01),
+                            metrics=['accuracy'])
+        model.summary()
+        return model
+
+
+
