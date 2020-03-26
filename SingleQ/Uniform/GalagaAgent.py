0ee22d311bcf723fc72ac4cf3d04b4d0ac2ae3f3
vat880 <jbg4a@mtmail.mtsu.edu>
2020-03-26 05:26:07 -0500

    Added get_action function and adjusted other functions. Removed redundant variables

--- SingleQ/Uniform/GalagaAgent.py
+++ SingleQ/Uniform/GalagaAgent.py
@@ -6,36 +6,44 @@ import numpy as np
 
 sys.path.append('../../') # Get top-level
 from HyperParameters import *
+from utils import map_actions
 
-# ten enemy types and one projectile type so 11 feature detectors
 
 class GalagaAgent:
         def __init__(self, action_size, image_width, image_height, num_channels):
+            #variables used to set input shape for model
             self.action_size = action_size
             self.image_width = image_width
             self.image_height = image_height
             self.num_channels = num_channels
-            #variables used to set input shape for Conv2D layer
-            self.model = make_model(self.image_height, self.image_width, self.num_channels, self.action_size)  
+            #creates model and stores it
+            self.model = self.build_model()  
         
+        #wrapper function for set_weights
         def set_weights(self,weights):
-            self.model.set_weights(weights)
+            return self.model.set_weights(weights)
 
+        #wrapper function for get_weights
         def get_weights(self):
-            self.model.get_weights()
+            return self.model.get_weights()
         
+        #wrapper function for fit function
         def fit(self, start_states, target_state):
-            self.model.fit(start_states, target_state,
+            return self.model.fit(start_states, target_state,
                            batch_size = 'BATCHES',
                            epochs = 'EPOCHS',
-                           verbose = 1)
-        
-        def get_action(self,):
+                           verbose = 0)
             
-            
-        def __make_model(rows, cols, channels, action_size):
+        #takes a state and returns an appropriate action
+        #uses utils.py map_action to map result to proper input 
+        def get_action(self, current_state):
+            return map_actions(np.argmax(self.model.predict(current_state)))
+
+        def build_model(self):
             model = keras.Sequential()
-            model.add(keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu', strides=4, input_shape = [rows, cols,  channels]))
+            model.add(keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu', strides=4, input_shape = [self.image_height,
+                                                                                                               self.image_width,
+                                                                                                               self.num_channels]))
             model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
             model.add(keras.layers.Conv2D(32, kernal_size=(3, 3), activation='relu', strides=2))
             model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
@@ -44,7 +52,7 @@ class GalagaAgent:
             model.add(keras.layers.Flatten())
             model.add(keras.layers.Dense(512, activation='relu'))
             model.add(keras.layers.dropout(.30)
-            model.add(keras.layers.Dense(action_size, activation='softmax'))
+            model.add(keras.layers.Dense(self.action_size, activation='softmax'))
 
             model.compile(loss=keras.losses.categorical_crossentropy, 
                           optimizer=keras.optimizers.Nadam('LEARNING_RATE'),
