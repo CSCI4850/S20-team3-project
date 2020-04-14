eec61626ee6243ca9e31bf1844ea3f553eebb786
Nathan Byrnes <ndb3w@mtmail.mtsu.edu>
2020-04-14 12:30:41 -0500

    DoubleQ Prioritized

--- DoubleQ/Prioritized/GalagaAgent.py
+++ DoubleQ/Prioritized/GalagaAgent.py
@@ -18,6 +18,7 @@ class GalagaAgent:
             self.num_channels = num_channels
             #creates model and stores it
             self.model = self.build_model()  
+            self.importance_weights = []
             
         #wrapper function for set_weights
         def set_weights(self,weights):
@@ -47,7 +48,7 @@ class GalagaAgent:
                            verbose = 0)
 
         def predict(self, states):
-            n_states = states.shape[0] if (states.ndim is 4 and not params['GRAYSCALE']) or (states.ndim is 3 and params['GRAYSCALE']) else 1 # if single state passed, expand to (1, w, h, c), else (n, w, h, c)
+            n_states = states.shape[0] if (states.ndim is 3 and not params['GRAYSCALE']) or (states.ndim is 3 and params['GRAYSCALE']) else 1 # if single state passed, expand to (1, w, h, c), else (n, w, h, c)
             states = states.reshape(n_states, params['IMG_WIDTH'], params['IMG_HEIGHT'],
                                                   1 if params['GRAYSCALE'] else 3)
             return self.model.predict(states)
@@ -60,19 +61,34 @@ class GalagaAgent:
             Q = self.model.predict(current_state)
             return map_actions(np.argmax(Q)), Q
 
+        def huber_loss(self, target, prediction):
+            delta = params['HUBER_DELTA']
+            loss = np.power(target - prediction, 2) if (target - prediction) <= delta else np.abs(target-prediction)
+            return self.importance_weights * loss
+
         def build_model(self):
             model = tf.keras.Sequential()
             model.add(tf.keras.layers.Conv2D(64, kernel_size=(7, 7), activation='relu', input_shape = [self.image_width,
                                                                                                                self.image_height,
                                                                                                                self.num_channels]))
+            model.add(tf.keras.layers.MaxPooling2D(2,2))
             model.add(tf.keras.layers.Conv2D(32, kernel_size=(5, 5), activation='relu'))
+            model.add(tf.keras.layers.Conv2D(16, kernel_size=(5, 5), activation='relu'))
+            model.add(tf.keras.layers.MaxPooling2D(2,2))
             model.add(tf.keras.layers.Flatten())
-            model.add(tf.keras.layers.Dense(15, activation='relu'))
+            model.add(tf.keras.layers.Dense(1024, activation='relu'))
+            model.add(tf.keras.layers.Dense(512, activation='relu'))
             model.add(tf.keras.layers.Dense(self.action_size, activation='softmax'))
 
             model.compile(loss=tf.keras.losses.Huber(params['HUBER_DELTA']),
-                          optimizer=tf.keras.optimizers.Nadam(params['LEARNING_RATE']),
+                          optimizer=tf.keras.optimizers.Adam(params['LEARNING_RATE']),
                           metrics=['accuracy'])
             
             model.summary()
             return model
+
+        def get_summary(self):
+            summary = []
+            self.model.summary(print_fn=lambda x: summary.append(x))
+            summary_str = "\n".join(summary)
+            return summary_str
